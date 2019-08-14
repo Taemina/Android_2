a13c4140e85424663c237ae1d553861f47ceb2f3
Taemina <Olyzub.96@mail.ru>
2019-08-14 17:22:06 +0700

    сделала ДЗ к уроку №8

---
+++ MySMS/app/src/main/java/com/example/mysms/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mysms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendMessageBtn = findViewById(R.id.btnSendSMS);
+        final EditText messagetEt = findViewById(R.id.txtMessage);
+        final EditText mPhoneNoEt = findViewById(R.id.txtPhoneNo);
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = messagetEt.getText().toString();
+                String phoneNo = mPhoneNoEt.getText().toString();
+                if (!TextUtils.isEmpty(message) && !TextUtils.isEmpty(phoneNo)) {
+                    Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + phoneNo));
+                    smsIntent.putExtra("sms_body", message);
+                    startActivity(smsIntent);
+                }
+            }
+        });
+
+
+    }
+}
